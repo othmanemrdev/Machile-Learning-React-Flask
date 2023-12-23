9e2331db81cfcb1aa9b6193e548a9e4a2001f5ef
othmanemrdev <othmanehilal200255@gmail.com>
2023-12-23 18:52:51 +0100

    first commit

---
+++ src/Pages/ResultPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ResultPage = () => {
+  const location = useLocation();
+  const prediction = location.state?.prediction || 'No prediction';
+  const isCreditApproved = prediction.includes(0);
+  console.log('Prediction0 in ResultPage:', location.state?.prediction);
+  console.log('Prediction in ResultPage:', prediction);
+
+  return (
+    <div className="result-page-container">
+      <h1>Result Page</h1>
+      {isCreditApproved ? (
+        <p className="result-message success">Le prêt est approuvé. Félicitations !</p>
+      ) : (
+        <p className="result-message failure">Désolé, le prêt n'est pas approuvé en raison d'un défaut de crédit.</p>
+      )}
+    </div>
+  );
+};
+
+export default ResultPage;
